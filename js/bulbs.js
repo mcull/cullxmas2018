848b4bcb91fd098661ef7e1cce7af44e5a1db6a7
atticus cull <atticus@atticuss-MacBook-Pro.local>
2018-12-16 18:39:28 -0800

    changed bulb

--- js/bulbs.js
+++ js/bulbs.js
@@ -1,19 +1,17 @@
 
-  function lightbulb(lightcolor, x, y, rotation) {
+  function lightbulb(lightcolor, rotation) {
     this.color = lightcolor;
-    this.x = x;
-    this.y = y;
     //this.rotation = rotation;
     this.brightness = floor(random(128,256));
     this.direction = 1;
-    this.render = function() {
+    this.render = function(x, y) {
       noStroke();
       fill(color(red(this.color), green(this.color), blue(this.color), this.brightness));
       if(this.brightness >= 256 || this.brightness <= 128) {
         this.direction *= -1
       }
       this.brightness += 1 * this.direction;
-      ellipse(this.x, this.y, 8 + 5 * this.brightness/255, 8 + 5 * this.brightness/255);
+      ellipse(x, y, 8 + 5 * this.brightness/255, 8 + 5 * this.brightness/255);
 
     }
 
