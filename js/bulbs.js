a0f64bcd363b3a7bdec41e54f75d3d618da742f4
Marc Cull <marc@anyroad.com>
2018-12-16 20:32:54 -0800

    madness

--- js/bulbs.js
+++ js/bulbs.js
@@ -1,18 +1,21 @@
 
   function lightbulb(lightcolor, rotation) {
     this.color = lightcolor;
+    this.minBrightness = 80;
+    this.maxBrightness = 230;
+
     //this.rotation = rotation;
-    this.brightness = floor(random(128,256));
+    this.brightness = floor(random(this.minBrightness,this.maxBrightness));
     this.direction = 1;
-    this.render = function(x, y) {
+    this.render = function(x,y) {
       noStroke();
       fill(color(red(this.color), green(this.color), blue(this.color), this.brightness));
-      if(this.brightness >= 256 || this.brightness <= 128) {
+      if(this.brightness >= this.maxBrightness || this.brightness <= this.minBrightness) {
         this.direction *= -1
       }
-      this.brightness += 1 * this.direction;
-      ellipse(x, y, 8 + 5 * this.brightness/255, 8 + 5 * this.brightness/255);
-
+      this.brightness += random(0,3) * this.direction;
+      var radius = 8 + 3 * this.brightness/this.maxBrightness
+      ellipse(x, y+7, radius, radius+6);
     }
 
   }
--- js/sketch.js
+++ js/sketch.js
@@ -5,45 +5,97 @@ var animationDirection = 1;
 var animationSteps = 10;
 var animationStepPixels;
 var animationMask;
+var colors = ["red",
+              "blue",
+              "green",
+              "yellow",
+              "orange",
+              "purple"];
+var imageLoading = false;
+var imageIndex = 0;
+var images = ["./images/Christmas_Template_462446_3840x2400.jpg",
+        "./images/wood.jpeg"]
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  init();
+  strings = new Array();
+  for (var i = 0; i < 5; i++) {
+    strings.push(new stringOfLights(i,this.getAlternatingLights(random(9,11))));
+  }
 }
 
-function init() {
-  animationStepPixels = windowWidth/animationSteps;
-  string = new Array();
-  bulbs = new Array();
+function getAlternatingLights(num) {
+  var colorPair = new Array();
+  var lights = new Array();
+  while (colorPair.length < 2) {
+    var c = colors[Math.floor(Math.random() * colors.length)];
+    if (colorPair[0] == c) {
+      continue;
+    }
+    colorPair.push(c);
+  }
+  for (var i = 0; i < num; i++) {
+    lights.push(new lightbulb(color(colorPair[i%2])));
+  }
+  return lights;
 }
 
 function draw() {
-  background(204);
-  /*
-  if (stringX < windowHeight) {
-    if (isAnimating) {
-      animationMask
-    }
-    addString();
-  }*/
-  noFill()
-
-  bezier(0, 0, windowWidth/3, 150, (windowWidth/3)*2, 160, windowWidth, 100);
-  fill("red");
-  var steps = 10;
-  for (var i = 0; i <= steps; i++) {
-    var t = i / steps;
-    var x = bezierPoint(0, windowWidth/3, (windowWidth/3)*2, windowWidth, t);
-    var y = bezierPoint(0, 150, 160, 100, t);
-    ellipse(x, y, 5, 5);
+  //background(204);
+  if (!imageLoading) {
+    imageLoading = true;
+    loadImage(images[imageIndex++%2], function(img) {
+      background(img);
+      imageLoading = false;
+    });
   }
+  noFill();
+  strings.forEach(function(s) {
+    s.render();
+  });
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-  init();
 }
 
-function addAString() {
+function stringOfLights(index, bulbs) {
+  this.index = index;
+  this.bulbs = bulbs;
+  this.xoffset = random(30,60);
 
+  this.colors = colors;
+  this.hang = random(100,150);
+  this.yoffset = random(-10,10);
+
+  this.increment = function() {
+    return  this.yoffset + windowHeight/(strings.length+1);
+  }
+
+  this.top = function() {
+    return this.increment()*this.index;
+  }
+
+  this.startY = function() {
+    return (this.index % 2 == 0) ? this.top() : this.top() + this.increment();
+  }
+
+  this.endY = function() {
+    return (this.index % 2 > 0) ? this.top() : this.top() + this.increment();
+  }
+
+  this.render = function() {
+    strokeWeight(2)
+    stroke("black");
+    noFill();
+
+    var dangle = (this.startY() + this.endY())/2 + this.hang;
+    bezier(-this.xoffset, this.startY(), windowWidth/3, dangle, (windowWidth/3)*2, dangle, windowWidth+this.xoffset, this.endY());
+    for (var i = 0; i < bulbs.length; i++) {
+      var t = i / bulbs.length;
+      var x = bezierPoint(-this.xoffset, windowWidth/3, (windowWidth/3)*2, windowWidth+this.xoffset, t);
+      var y = bezierPoint(this.startY(), dangle, dangle, this.endY(), t);
+      bulbs[i].render(x,y);
+    }
+  }
 }
